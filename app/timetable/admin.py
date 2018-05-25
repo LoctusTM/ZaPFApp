b5f603ada9ca627bffd3c2bbef2117e9ecb2cf1a
ethylomat <ethylomat@c6h12o6.de>
2018-05-25 19:54:13 +0200

    bla

--- app/timetable/admin.py
+++ app/timetable/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 
 from .models import Timeslot, Room, AKSlot, AK, RoomAssignment, News
 
+class AKAdmin(admin.ModelAdmin):
+    list_display = ('akslot', 'name', 'short', 'responsible', 'room', 'url', 'backup', 'public_id', 'published', 'kleine_fachschaften')
+    
 admin.site.register(Timeslot)
 admin.site.register(Room)
 admin.site.register(AKSlot)
-admin.site.register(AK)
+admin.site.register(AK, AKAdmin)
 admin.site.register(RoomAssignment)
 admin.site.register(News)
