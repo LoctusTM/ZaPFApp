eba6f865eb7e9cb81d09dc818a43feb41cc9291d
ethylomat <ethylomat@c6h12o6.de>
2018-05-25 19:57:48 +0200

    bla

--- app/timetable/admin.py
+++ app/timetable/admin.py
@@ -4,8 +4,8 @@ from .models import Timeslot, Room, AKSlot, AK, RoomAssignment, News
 
 class AKAdmin(admin.ModelAdmin):
     list_display = ('akslot', 'name', 'short', 'responsible', 'room', 'url', 'backup', 'public_id', 'published', 'kleine_fachschaften')
-    search_fields = ('akslot', 'name', 'short', 'responsible', 'room', 'url', 'backup', 'public_id', 'published', 'kleine_fachschaften')
-    
+    search_fields = ('name', 'short', 'responsible', 'room')
+
 admin.site.register(Timeslot)
 admin.site.register(Room)
 admin.site.register(AKSlot)
