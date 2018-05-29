6173c8bcea273ef89386025a8d92a25b1f3cb2fa
ethylomat <ethylomat@c6h12o6.de>
2018-05-29 10:12:30 +0200

    yolo

--- app/timetable/admin.py
+++ app/timetable/admin.py
@@ -5,9 +5,13 @@ from .models import Timeslot, Room, AKSlot, AK, RoomAssignment, News
 class AKAdmin(admin.ModelAdmin):
     list_display = ('name', 'short', 'akslot', 'room', 'responsible', 'backup', 'public_id', 'published', 'kleine_fachschaften')
 
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('time', 'message')
+
+
 admin.site.register(Timeslot)
 admin.site.register(Room)
 admin.site.register(AKSlot)
 admin.site.register(AK, AKAdmin)
 admin.site.register(RoomAssignment)
-admin.site.register(News)
+admin.site.register(News, NewsAdmin)
